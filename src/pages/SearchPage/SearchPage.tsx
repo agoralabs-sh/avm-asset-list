2bfb4df5ff91385d343c0cd494bead55689841b4
Kieran O'Neill <hello@kieranoneill.com>
2024-02-22 10:53:30 +0200

    feat: display uri below qr code (#5)
    * ci: use node.js 20+ in github actions
    
    * feat: use svg for qr code and add uri to display

--- src/pages/SearchPage/SearchPage.tsx
+++ src/pages/SearchPage/SearchPage.tsx
@@ -3,7 +3,8 @@ import {
   decode as decodeBase64,
   encodeURLSafe as encodeBase64URLSafe,
 } from '@stablelib/base64';
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { NavigateFunction, useNavigate } from 'react-router-dom';
 
 // components
@@ -12,13 +13,17 @@ import AssetSelect from '@app/components/AssetSelect';
 // constants
 import { ASSET_ROUTE, CHAIN_ROUTE, DEFAULT_GAP } from '@app/constants';
 
+// features
+import { setTitle } from '@app/features/system';
+
 // types
-import type { IAsset } from '@app/types';
+import type { IAppThunkDispatch, IAsset } from '@app/types';
 
 // utils
 import createAssetList from '@app/utils/createAssetList';
 
 const SearchPage: FC = () => {
+  const dispatch: IAppThunkDispatch = useDispatch<IAppThunkDispatch>();
   const navigate: NavigateFunction = useNavigate();
   // misc
   const assets: IAsset[] = createAssetList();
@@ -27,6 +32,10 @@ const SearchPage: FC = () => {
     navigate(
       `/${CHAIN_ROUTE}/${encodeBase64URLSafe(decodeBase64(value.network.genesisHash))}/${ASSET_ROUTE}/${value.id}`
     );
+
+  useEffect(() => {
+    dispatch(setTitle(__APP_TITLE__));
+  }, []);
 
   return (
     <VStack
